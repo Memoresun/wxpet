6b185578815c0dfb306696a3beb47438ad57b812
w-memoresun <memoresun@gmail.com>
2018-04-23 20:47:25 +0800

    update

---
+++ pages/index/index.js
@@ -0,0 +1,111 @@
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    slide: ['http://chongyouchong.com/uploadfile/seller/image/20180312/index-banner-img.png',
+      'http://chongyouchong.com/uploadfile/seller/image/20180312/index-banner-img.png'
+    ],
+    banner: ['http://static.memorecool.cn/wx/pet/banner1.jpg',
+      'http://static.memorecool.cn/wx/pet/banner2.jpg',
+      'http://static.memorecool.cn/wx/pet/banner3.jpg',
+      'http://static.memorecool.cn/wx/pet/banner4.jpg',
+      'http://static.memorecool.cn/wx/pet/banner5.jpg',
+      'http://static.memorecool.cn/wx/pet/banner6.jpg',
+      'http://static.memorecool.cn/wx/pet/banner7.jpg',
+      'http://static.memorecool.cn/wx/pet/banner8.jpg',
+      'http://static.memorecool.cn/wx/pet/banner9.jpg',
+      'http://static.memorecool.cn/wx/pet/banner10.jpg',
+      'http://static.memorecool.cn/wx/pet/banner11.jpg',
+    ],
+    nav: [{
+      icon: '/images/index-icon-1.png',
+      name: '犬舍'
+    }, {
+      icon: '/images/index-icon-2.png',
+      name: '猫舍'
+    }, {
+      icon: '/images/index-icon-4.png',
+      name: '主粮'
+    }, {
+      icon: '/images/index-icon-5.png',
+      name: '零食'
+    }, {
+      icon: '/images/index-icon-6.png',
+      name: '玩具'
+    }, {
+      icon: '/images/index-icon-7.png',
+      name: '商家积分榜'
+    }, {
+      icon: '/images/index-icon-8.png',
+      name: '医疗保健'
+    }, {
+      icon: '/images/index-icon-9.png',
+      name: '日用品',
+    }, {
+      icon: '/images/index-icon-11.png',
+      name: '拼团'
+    }, {
+      icon: '/images/index-icon-11.png',
+      name: '商家'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
